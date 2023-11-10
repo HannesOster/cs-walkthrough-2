96e19b4cb1b8c35646a0fc80db7fdbee0954a0c1
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-11-10 20:10:39 +0100

    converting

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿long bigNumber = 10000;
+int smallNumber = 100;
+
+smallNumber = (int)bigNumber;
+
+string textNumber = "200";
+int number;
+
+number = Convert.ToInt32(textNumber);
+Console.WriteLine(number + smallNumber);
